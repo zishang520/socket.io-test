c9d444a1fa615ea68206743de5797bb9ea3348a5
Luoyy <zishang520@gmail.com>
2022-07-08 16:04:26 +0800

    add

--- socket.io.go
+++ socket.io.go
@@ -4,22 +4,47 @@ import (
 	"github.com/zishang520/engine.io/types"
 	"github.com/zishang520/engine.io/utils"
 	"github.com/zishang520/socket.io/socket"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"regexp"
 	"syscall"
 )
 
 func main() {
 	utils.Log().DEBUG = true
+	go func() {
+		utils.Log().Success("%v", http.ListenAndServe("localhost:6060", nil))
+	}()
 	c := socket.DefaultServerOptions()
 	c.SetAllowEIO3(true)
 	c.SetCors(&types.Cors{
 		Origin:      "http://127.0.0.1:8000",
 		Credentials: true,
 	})
-	utils.Log().Success("Handshake：%v", c.ServerOptions.AllowEIO3())
+	utils.Log().Success("AllowEIO3：%v", c.ServerOptions.AllowEIO3())
 	httpServer := types.CreateServer(nil)
 	io := socket.NewServer(httpServer, c)
+	io.Of(
+		regexp.MustCompile(`/\w+`),
+		nil,
+	).On("connection", func(clients ...interface{}) {
+		client := clients[0].(*socket.Socket)
+		utils.Log().Success("/ tets Handshake：%v", client.Handshake())
+		client.Broadcast().Emit("hi tets")
+		client.On("event", func(clients ...interface{}) {
+			utils.Log().Success("/ tets eventeventeventeventevent%v", clients)
+		})
+		client.On("disconnect", func(...interface{}) {
+			utils.Log().Success("/ tets disconnect")
+		})
+		client.On("chat message", func(msgs ...interface{}) {
+			io.Of("/test", nil).Emit("hi", msgs...)
+			client.Emit("chat message", msgs...)
+			utils.Log().Success("/ tets message：%v", msgs[0])
+		})
+	})
 	io.On("connection", func(clients ...interface{}) {
 		client := clients[0].(*socket.Socket)
 		utils.Log().Success("Handshake：%v", client.Handshake())
@@ -34,6 +59,9 @@ func main() {
 			io.Of("/test", nil).Emit("hi", msgs...)
 			client.Emit("chat message", msgs...)
 			utils.Log().Success("message：%v", msgs[0])
+			utils.Log().Success("FetchSockets %v", io.Adapter().FetchSockets(&socket.BroadcastOptions{
+				Rooms: types.NewSet[socket.Room]("/"),
+			}))
 		})
 	})
 	httpServer.Listen("127.0.0.1:9999", nil)
